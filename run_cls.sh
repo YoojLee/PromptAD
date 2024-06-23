fff68a4ffb77c504fd0fbd625482aeb8f163499b
YoojLee <daebaq27@gmail.com>
2024-06-23 17:13:49 +0000

    add a loop for seeds

--- run_cls.sh
+++ run_cls.sh
@@ -3,13 +3,13 @@
 # Define the DATASETS, SHOTS, CLASSES, and VISA_CLASSES arrays
 DATASETS=('mvtec' 'visa')
 SHOTS=(1 2 4)
-MVTEC_CLASSES=('pill' 'carpet' 'grid' 'leather' 'tile' 'wood'
-               'bottle' 'cable' 'capsule' 'hazelnut' 'metal_nut'
+MVTEC_CLASSES=('cable' 'capsule' 'pill' 'carpet' 'grid' 'leather' 'tile' 'wood'
+               'bottle' 'hazelnut' 'metal_nut'
                'screw' 'toothbrush' 'transistor' 'zipper')
 VISA_CLASSES=('candle' 'capsules' 'cashew' 'chewinggum'
               'fryum' 'macaroni1' 'macaroni2' 'pcb1' 'pcb2'
               'pcb3' 'pcb4' 'pipe_fryum')
-SEED=1
+SEEDS=(111 1)
 
 # Function to get the classes based on dataset
 get_classes() {
@@ -27,17 +27,19 @@ get_classes() {
 }
 
 # Loop through each dataset
-for DATASET in "${DATASETS[@]}"; do
-    # Loop through each shot
-    for SHOT in "${SHOTS[@]}"; do
-        # Get the appropriate classes for the current dataset
-        CLASSES=($(get_classes "$DATASET"))
-        # Loop through each class
-        for CLS in "${CLASSES[@]}"; do
-            # Echo the current SHOT and CLS
-            echo "Running with SHOT=$SHOT and CLS=$CLS"
-            # Run the Python script with the current dataset, shot, and class
-            python train_cls.py --dataset "$DATASET" --k-shot "$SHOT" --class_name "$CLS" --seed "$SEED"
+for SEED in "${SEEDS[@]}"; do
+    for DATASET in "${DATASETS[@]}"; do
+        # Loop through each shot
+        for SHOT in "${SHOTS[@]}"; do
+            # Get the appropriate classes for the current dataset
+            CLASSES=($(get_classes "$DATASET"))
+            # Loop through each class
+            for CLS in "${CLASSES[@]}"; do
+                # Echo the current SHOT and CLS
+                echo "Running with SHOT=$SHOT and CLS=$CLS (seed $SEED)"
+                # Run the Python script with the current dataset, shot, and class
+                python train_cls.py --dataset "$DATASET" --k-shot "$SHOT" --class_name "$CLS" --seed "$SEED"
+            done
         done
     done
 done
